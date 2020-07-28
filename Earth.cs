9f79e7fdea53c609f39300223d3f2197207e04ea
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2020-07-28 15:13:33 -0400

    Move classes into their own files

---
+++ Earth.cs
@@ -0,0 +1,11 @@
+namespace MoonPhaseConsole
+{
+    public static class Earth
+    {
+        public enum Hemispheres
+        {
+            Northern,
+            Southern
+        }            
+    }
+}
